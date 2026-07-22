7926727447e9fc3db22f41bdd9a27c1bd577bb3e
Krishna Pandita <kpandita68@gmail.com>
2026-07-22 22:59:42 +0530

    chore: remove backend test scratch files

--- backend/test_delete.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function run() {
-  await prisma.vehicle.deleteMany({ where: { make: 'Test' } });
-}
-run();
